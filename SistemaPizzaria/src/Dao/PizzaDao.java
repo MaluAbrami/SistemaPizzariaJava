8d62325d67fcff3b27671c21add8c424a5113871
aluno <aluno@LAB>
2024-11-28 20:13:04 -0300

    corrigindo interface Dao da Pizza

--- SistemaPizzaria/src/Dao/PizzaDao.java
+++ SistemaPizzaria/src/Dao/PizzaDao.java
@@ -5,7 +5,7 @@ import sistemapizzaria.Pizza;
 public interface PizzaDao {
     void adicionarPizza(Pizza p);
     Pizza procurarPizzaPorId(int id);
-    void atualizarPizza(Pizza p);
+    void atualizarPizza(int id, String nome, double valor, String ingredientes, int quantidade);
     boolean deletePizza(int id);
     String listAllPizzas();
 }
